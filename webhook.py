08366bf98345b7470e2f5bfb551391875b376cbc
xingshining <xingshining@360.cn>
2024-09-06 18:20:04 +0800

    update webhook

--- webhook.py
+++ webhook.py
@@ -58,4 +58,4 @@ def webhook():
         return 'Internal Server Error', 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
